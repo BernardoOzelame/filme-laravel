81d2f04bc5106509c4f73b0a800b7a392cf6142f
Sharlyne Rodrigues de Souza <sharlynerodrigues2@gmail.com>
2024-09-13 11:11:32 -0300

    Tela de login finalizada

--- resources/views/usuarios/login.blade.php
+++ resources/views/usuarios/login.blade.php
@@ -1,25 +1,39 @@
-@if($errors->any())
-    <div>
-        <h4>Preenche a porcaria do formulário</h4>
-        <ul>
-            @foreach($errors->all() as $erro)
-                <li>{{ $erro }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+@extends('base')
 
-<form action="{{ route('login') }}" method="post">
-    @csrf
-    <div>
-        <label for="username">Usuário</label>
-        <input id="username" name="username" type="text" required placeholder="Usuário" aria-label="Usuário">
-    </div>
-    <div>
-        <label for="password">Senha</label>
-        <input id="password" name="password" type="text" required placeholder="Senha" aria-label="Senha">
-    </div>
-    <div>
-        <button type="submit">Entrar</button>
-    </div>
-</form>
+@section('titulo', 'Login')
+
+@section('conteudo')
+
+@if($errors->any())
+        <div class="alert alert-danger m-5 fixed-top">
+            <h4>Ocorreu o(s) seguinte(s) erro(s):</h4>
+            <ul>
+                @foreach($errors->all() as $erro)
+                    <li>{{ $erro }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
+<div class="container d-flex justify-content-center align-items-center flex-column" style="height: 100vh;">
+    <h1 class="text-center">LOGIN</h1>
+    <form action="{{ route('login') }}" method="post">
+        @csrf
+        <div  style="margin-top: 10px">
+            <div class="mb-3">
+                <label for="username">Usuário</label>
+                <input id="username" name="username" type="text" placeholder="Usuário" aria-label="Usuário" class="form-control w-100">
+            </div>
+            <div class="mb-4">
+                <label for="password">Senha</label>
+                <input id="password" name="password" type="text" placeholder="Senha" aria-label="Senha" class="form-control w-100">
+            </div>
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary">Entrar</button>
+            </div>
+        </div>
+    </form>
+</div>
+
+@endsection
